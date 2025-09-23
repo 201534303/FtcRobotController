e3879c0632149ec681d3a582f514e3a9d1471140
Beatriz <beatriz@fornasaro.com>
2025-09-23 18:37:14 -0400

    Create StarterBotCode.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/StarterBotCode.java
@@ -0,0 +1,82 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@TeleOp
+public class StarterBotCode extends LinearOpMode {
+    boolean last = false;
+    boolean shoot = false;
+
+
+
+    @Override
+    public void runOpMode() {
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");//0 chb
+        DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");//1 chb
+        DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");//0 exh
+        DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");//1 exh
+
+        DcMotor shooterMotor = hardwareMap.dcMotor.get("shooterMotor");//2 exh
+
+        frontRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        backRightMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        shooterMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        IMU imu = hardwareMap.get(IMU.class, "imu");
+        IMU.Parameters parameters = new IMU.Parameters(new RevHubOrientationOnRobot(
+                RevHubOrientationOnRobot.LogoFacingDirection.UP,
+                RevHubOrientationOnRobot.UsbFacingDirection.FORWARD));
+        imu.initialize(parameters);
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x;
+            double rx = gamepad1.right_stick_x;
+
+            if (gamepad1.options) {
+                imu.resetYaw();
+            }
+
+
+            if(gamepad1.right_bumper && !last){
+                shoot = !shoot;
+
+                if(shoot){
+                    shooterMotor.setPower(0.7);
+                }
+                else{
+                    shooterMotor.setPower(0);
+                }
+            }
+            last = gamepad1.right_bumper;
+
+            double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
+
+            double rotX = x * Math.cos(-botHeading) - y * Math.sin(-botHeading);
+            double rotY = x * Math.sin(-botHeading) + y * Math.cos(-botHeading);
+
+            rotX = rotX * 1.1;
+
+            double denominator = Math.max(Math.abs(rotY) + Math.abs(rotX) + Math.abs(rx), 1);
+            double frontLeftPower = (rotY + rotX + rx) / denominator;
+            double backLeftPower = (rotY - rotX + rx) / denominator;
+            double frontRightPower = (rotY - rotX - rx) / denominator;
+            double backRightPower = (rotY + rotX - rx) / denominator;
+
+            frontLeftMotor.setPower(frontLeftPower);
+            backLeftMotor.setPower(backLeftPower);
+            frontRightMotor.setPower(frontRightPower);
+            backRightMotor.setPower(backRightPower);
+        }
+    }
+}
